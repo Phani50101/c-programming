5a23e6382ad5e07f6bebbebc8ffd32266c404655
Phani50101 <113985082+Phani50101@users.noreply.github.com>
2022-10-05 15:51:29 +0530

    Add files via upload

---
+++ doller to indian rupees.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float d,i,c;
+	printf("enter number of the dollers: ");
+	scanf("%f",&d);
+	c=d*81.26;
+	printf("the value of doller to indian rupess is:");
+	printf("%f",c);
+	
+}
